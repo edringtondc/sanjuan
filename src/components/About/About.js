984425ff669591eb0f45f1f45af8e11084ac12ee
Elisabeth Edrington <edringtondc@gmail.com>
2019-05-10 12:13:53 -0600

    added images

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,12 +1,13 @@
 import React from "react";
 import classes from "./about.module.css"
+import team from "../../assets/images/team.jpg"
 
 const About = () => {
     return (
         <div className={classes.about}>
             <h3>Welcome to the Wellness Center at San Juan Chiropractic</h3>
             <div >Macaroon tart jelly marshmallow. Marzipan dessert bonbon. Sugar plum jelly cake lollipop. Jujubes biscuit fruitcake jujubes marzipan. Marzipan macaroon toffee soufflé sesame snaps. Tootsie roll chocolate bar wafer fruitcake macaroon soufflé danish. Cupcake tiramisu cookie. Candy canes danish candy candy canes. Candy sweet sesame snaps marshmallow sweet pastry sweet roll lollipop. Ice cream marzipan chocolate bar. Jelly-o candy danish bonbon cotton candy biscuit. Dragée dragée liquorice tiramisu apple pie ice cream gingerbread halvah.</div>
-
+            <img src={team} alt="Our team"/>
         </div>
     )
 }
