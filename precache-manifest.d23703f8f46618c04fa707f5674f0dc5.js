3f7765de2760ab80559edb88859a1ec3ccbeaa28
Elisabeth Edrington <edringtondc@gmail.com>
2019-05-14 18:24:21 -0600

    Updates

---
+++ precache-manifest.d23703f8f46618c04fa707f5674f0dc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4abc51475ff91f5699730281e62960a2",
+    "url": "/sanjuan/index.html"
+  },
+  {
+    "revision": "4c0d849b2d011d269e9d",
+    "url": "/sanjuan/static/css/main.ba5cf9f6.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/sanjuan/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "4c0d849b2d011d269e9d",
+    "url": "/sanjuan/static/js/main.4e30a530.chunk.js"
+  },
+  {
+    "revision": "834793259c5a07136583",
+    "url": "/sanjuan/static/js/runtime~main.36b31988.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sanjuan/static/media/logo.5d5d9eef.svg"
+  }
+]);
