baf0404cf358b395b13d544df5d8d43951fa971c
Elisabeth Edrington <edringtondc@gmail.com>
2019-05-14 19:29:51 -0600

    moved layouts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Layout from "./components/Layout/Layout"
+import Layout from "./Layout/Layout"
 import "./App.css"
 
 
@@ -7,6 +7,7 @@ import "./App.css"
 class App extends Component {
   render() {
     return (
+      
       <div>
         <Layout />
 
---
+++ src/Layout/Layout.js
@@ -0,0 +1,43 @@
+import React, {Component } from "react";
+import Aux from "../hoc/Aux/Aux"
+import About from "../components/About/About"
+import Main from "../components/Main/Main"
+import Toolbar from "../components/Navigation/Toolbar/Toolbar"
+import Team from "../components/Team/Team"
+import ScheduleNow from "../components/ScheduleNow/ScheduleNow"
+import Testimonials from "../components/Testimonials/Testimonials"
+import Visit from "../components/Visit/Visit"
+import Footer from "../components/Footer/Footer"
+import MainMenu from "../components/MainMenu/MainMenu"
+
+
+// import classes from "./Layout.css"
+
+
+class Layout extends Component {
+ 
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar/>
+
+            
+                <Main/>
+                <MainMenu/>
+                <ScheduleNow/>
+                <Testimonials/>
+                <Visit />
+                <About/>
+      
+              
+                <Footer/>
+
+
+            </Aux>
+        )
+    }
+
+}
+
+export default Layout;
