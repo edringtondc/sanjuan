484f68085f09b59de723c7debf030186b2e74ade
Elisabeth Edrington <edringtondc@gmail.com>
2019-05-16 15:47:39 -0600

    embeded map

--- src/components/Visit/Visit.js
+++ src/components/Visit/Visit.js
@@ -42,14 +42,11 @@ const visit = () => {
                     </ul>
                 </div>
                 <div className={classes.map}>
-                    <h3 className={classes.hoursTitle} > Location </h3>
-                    <p>
-                    164 Colorado Avenue, Unit C
-                    Montrose, CO 81401</p>
-                    <a href="https://www.google.com/maps/place/San+Juan+Chiropractic/@38.470593,-107.8709987,15z/data=!4m5!3m4!1s0x0:0x5c28b59f1de75013!8m2!3d38.470592!4d-107.870983"
-                        target="_blank" rel="noopener noreferrer" >
-                        <img src={Map} className={classes.map} alt="map of SJC" />
-                    </a>
+           
+                 
+                
+
+                    <iframe class={classes.iframe} src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12494.954869725958!2d-107.8709987!3d38.470593!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5c28b59f1de75013!2sSan+Juan+Chiropractic!5e0!3m2!1sen!2sus!4v1558043064832!5m2!1sen!2sus" allowfullscreen></iframe>
                 </div>
             </div>
         </div >
