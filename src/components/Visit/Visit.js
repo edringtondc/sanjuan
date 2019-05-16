c849f14b2663fedffe03033f0a3ec94bfe855f0b
Elisabeth Edrington <edringtondc@gmail.com>
2019-05-16 11:52:58 -0600

    vist us, map is a link and accurate, added hours

--- src/components/Visit/Visit.js
+++ src/components/Visit/Visit.js
@@ -1,18 +1,50 @@
 import React from "react";
 import classes from "./Visit.module.css"
-import Map from "../../assets/images/google.png"
+import Map from "../../assets/images/SJCMap.png"
 
 const visit = () => {
-    
+
     return (
-        <div className={classes.visit}>
-        <div className={classes.map}>  <img src={Map} className={classes.map}></img> </div>
-      
-     
-       
-    </div>
+        <div className={classes.wrapper}>
+            <h1>
+                Visit Us
+            </h1>
+            <div className={classes.visit}>
+
+                <div className={classes.map}>
+                    <a href="https://www.google.com/maps/place/San+Juan+Chiropractic/@38.470593,-107.8709987,15z/data=!4m5!3m4!1s0x0:0x5c28b59f1de75013!8m2!3d38.470592!4d-107.870983"
+                        target="_blank" rel="noopener noreferrer" >
+                        <img src={Map} className={classes.map} alt="map of SJC" />
+                    </a>
+                </div>
+                <div className={classes.info}>
+
+                <h3>Wellness Center Hours</h3>
+                    <ul>
+                        <li>
+                            Monday: 9:00am - 6:00pm
+                    </li>
+                        <li>
+                            Tuesday: 2:00pm - 8:00pm
+                    </li>
+                        <li>
+                            Wednesday: 9:00am - 6:00pm
+                    </li>
+                        <li>
+                            Thursday: 2:00pm - 8:00pm
+                    </li>
+                        <li>
+                            Friday: 9:00am - 2:00pm
+                    </li>
+                        <li>
+                            Saturday (F/L): 9:00am - 11:00pm
+                    </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     )
-    
+
 }
 
 export default visit;
