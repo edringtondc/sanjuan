28bfdb26ceb77b6295eefebc9c882c4bf64ef143
Elisabeth Edrington <edringtondc@gmail.com>
2019-05-16 13:03:59 -0600

    updated location

--- src/components/Visit/Visit.js
+++ src/components/Visit/Visit.js
@@ -7,7 +7,7 @@ const visit = () => {
 
     return (
 
-     
+
 
         <div className={classes.wrapper}>
             <div className={classes.empty}>
@@ -16,11 +16,11 @@ const visit = () => {
 
             <div className={classes.visit}>
 
-             
+
                 <div className={classes.info}>
 
-                <h3>Wellness Center Hours</h3>
-                    <ul>
+                    <h3 className={classes.hoursTitle}>Wellness Center Hours</h3>
+                    <ul className={classes.hours}>
                         <li>
                             Monday: 9:00am - 6:00pm
                     </li>
@@ -42,13 +42,17 @@ const visit = () => {
                     </ul>
                 </div>
                 <div className={classes.map}>
+                    <h3 className={classes.hoursTitle} > Location </h3>
+                    <p>
+                    164 Colorado Avenue, Unit C
+                    Montrose, CO 81401</p>
                     <a href="https://www.google.com/maps/place/San+Juan+Chiropractic/@38.470593,-107.8709987,15z/data=!4m5!3m4!1s0x0:0x5c28b59f1de75013!8m2!3d38.470592!4d-107.870983"
                         target="_blank" rel="noopener noreferrer" >
                         <img src={Map} className={classes.map} alt="map of SJC" />
                     </a>
                 </div>
             </div>
-        </div>
+        </div >
     )
 
 }
